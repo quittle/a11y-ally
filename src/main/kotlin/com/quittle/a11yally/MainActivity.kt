e271630d57d58ecfaeb263e364c2270708a6a2c4
Dustin Toff <me@dustintoff.com>
2018-10-20 21:41:08 +0000

    Added a button for toggling the overlay service

--- src/main/kotlin/com/quittle/a11yally/MainActivity.kt
+++ src/main/kotlin/com/quittle/a11yally/MainActivity.kt
@@ -3,10 +3,22 @@ package com.quittle.a11yally
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ToggleButton
 
 public class MainActivity : Activity() {
     protected override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startService(Intent(this, OverlayService::class.java))
+
+        setContentView(R.layout.main_activity)
+
+        val overlayServiceIntent = Intent(this, OverlayService::class.java)
+        val toggleButton: ToggleButton = findViewById(R.id.enable_service_button)
+        toggleButton.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                startService(overlayServiceIntent)
+            } else {
+                stopService(overlayServiceIntent)
+            }
+        }
     }
 }
