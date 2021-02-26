69f63abbce97fcce88fd2db64f62ed0840e220ad
Dustin Toff <me@dustintoff.com>
2021-02-25 22:17:59 -0500

    Upgrade dependencies

--- app/src/test/kotlin/com/quittle/a11yally/activity/MainActivityTest.kt
+++ app/src/test/kotlin/com/quittle/a11yally/activity/MainActivityTest.kt
@@ -1,7 +1,10 @@
 package com.quittle.a11yally.activity
 
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
 
+@RunWith(RobolectricTestRunner::class)
 class MainActivityTest {
     @Test
     fun constructor() {
